ac6648613f7270a48f1d5c3420730f5050c89a9c
Mohammed Naguib <m.naguib2611@gmail.com>
2022-12-25 00:09:04 +0300

    added-test-database

--- migrations/sqls/20221224193817-orders-table-up.sql
+++ migrations/sqls/20221224193817-orders-table-up.sql
@@ -1,8 +1,13 @@
- CREATE TYPE valid_status AS ENUM ('active','complete');
-
+BEGIN;
+DO $$
+BEGIN
+ IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'valid_status') THEN
+    CREATE TYPE  valid_status AS ENUM ('active','complete');
+ END IF;
+ END
+$$;
 CREATE TABLE orders (status valid_status ,user_id INTEGER, FOREIGN KEY(user_id) REFERENCES users(id),id SERIAL PRIMARY KEY); 
-
-
+COMMIT;
 
   
  
